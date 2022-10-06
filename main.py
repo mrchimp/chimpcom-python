f789a958192f1236c5426b72734478eeaf3b3106
Jake Gully <chimpytk@gmail.com>
2022-10-06 01:07:22 +0100

    Allow single-shot commands

--- main.py
+++ main.py
@@ -22,19 +22,25 @@ def get_response(cmd_in, token):
     if r.status_code == 200:
         print(r.json()['cmd_out'])
     else:
-        print("Network error. Invalid URL?")
+        print("Error " + str(r.status_code) + ". Invalid URL?")
 
-
-username = input("Username: ")
 
 parser = argparse.ArgumentParser(description="Send Chimpcom command")
-parser.add_argument("command", nargs="?", help="command")
+parser.add_argument("--token", action=argparse.BooleanOptionalAction)
 parser.add_argument("--url", default="https://deviouschimp.co.uk/api/respond")
+parser.add_argument("--username", nargs="?")
+parser.add_argument("command", nargs="*")
 args = parser.parse_args()
+
+username = args.username
+
+if not username:
+    username = input("Username: ")
+
 service_id = "chimpcom"+args.url
 token = keyring.get_password(service_id, username)
 
-if not token or args.command == "token":
+if not token or args.token:
     token = getpass("Enter your token: ")
 
 if not token:
@@ -43,6 +49,12 @@ if not token:
 
 keyring.set_password(service_id, username, token)
 
+# Single command
+if args.command:
+    get_response(" ".join(args.command), token)
+    sys.exit()
+
+# Interactive
 while True:
     cmd_in = input("> ")
     if cmd_in == "exit":
