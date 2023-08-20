53d421191735f55e2140d2167d1b33421efe7149
Mr Chimp <chimpytk@gmail.com>
2023-08-20 21:15:20 +0100

    Update action system

--- README.md
+++ README.md
@@ -4,23 +4,44 @@ So there's a website that looks like a command line. So what else do we need? Wh
 
 # Setup
 
-1. Copy `main.py` to somewher on your `$PATH`. E.g.
+1. Copy `main.py` to somewhere on your `$PATH`. E.g.
 
-    ```sh
-    cp main.py /usr/local/bin/chimpcom
-    ```
+   ```sh
+   $ cp main.py /usr/local/bin/chimpcom
+   ```
 
 2. Get a token from [Chimpcom](https://deviouschimp.co.uk)
 
-    ```sh
-    $ token --create some_token_name
-    ```
+   ```sh
+   # Note: Run this on deviouschimp.co.uk not in your terminal!
+   $ token --create some_token_name
+   ```
 
 3. Enter your token.
 
-    ```sh
-    $ chimpcom token
-    # ...then follow the instructions
-    ```
+   ```sh
+   $ chimpcom token
+   # ...then follow the instructions
+   ```
 
 4. You shouldn't have to enter your token again.
+
+5. You might want to alias chimpcom with your username. E.g.
+
+   ```sh
+   alias chimpcom="chimpcom --username=AzureDiamond"
+   ```
+
+# Arguments
+
+- `--token`
+
+  Use this flag to set your token. Your token will be stored in your OS's keyring.
+
+- `--url`
+
+  The URL to use as the Chimpcom API. Defaults to `https://deviouschimp.co.uk/api/respond`.
+
+- `--username`
+
+  The Chimpcom username to use. This allows you to store multiple tokens.
--- main.py
+++ main.py
@@ -1,62 +1,115 @@
 #!/usr/bin/env python3
 
-import sys
-import requests
 import argparse
 import keyring
+import os
+import requests
+import subprocess
+import sys
+import tempfile
 from getpass import getpass
 
+class Chimpcom:
+    def __init__(self) :
+        self.action_id = None
+        parser = argparse.ArgumentParser(description="Send Chimpcom command")
+        parser.add_argument("--token", action=argparse.BooleanOptionalAction)
+        parser.add_argument("--url", default="https://deviouschimp.co.uk/api/respond")
+        parser.add_argument("--username", nargs="?")
+        parser.add_argument("command", nargs="*")
+        self.args = parser.parse_args()
 
-def get_response(cmd_in, token):
-    data = {"cmd_in": cmd_in, "format": "cli"}
-    headers = {
-        "Authorization": "Bearer " + token,
-        "Content-Type": "application/json",
-        "Accept": "application/json",
-        # TODO: would be better if we didn't need this...
-        "X-Requested-With": "XMLHttpRequest",
-    }
+        username = self.args.username
 
-    r = requests.post(args.url, headers=headers, json=data)
+        if not username:
+            username = input("Username: ")
 
-    if r.status_code == 200:
-        print(r.json()['cmd_out'])
-    else:
-        print("Error " + str(r.status_code) + ". Invalid URL?")
+        service_id = "chimpcom"+self.args.url
+        token = keyring.get_password(service_id, username)
+
+        if not token or self.args.token:
+            token = getpass("Enter your token: ")
+
+        if not token:
+            print("No token.")
+            sys.exit(1)
+
+        keyring.set_password(service_id, username, token)
+
+        # Single command
+        if self.args.command:
+            self.get_response(" ".join(self.args.command), token)
+            sys.exit()
+
+        # Interactive
+        while True:
+            cmd_in = input("> ")
+            if cmd_in == "exit":
+                sys.exit()
+            self.get_response(cmd_in, token)
+
+    def get_response(self, cmd_in, token):
+        data = {
+            "action_id": self.action_id,
+            "cmd_in": cmd_in,
+            "format": "cli"
+        }
+
+        headers = {
+            "Authorization": "Bearer " + token,
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+            "X-Requested-With": "XMLHttpRequest",
+        }
+
+        r = requests.post(self.args.url, headers=headers, json=data)
+
+        res = r.json();
+        self.action_id = res['action_id'];
+
+        if r.status_code == 200:
+            if res['edit_content']:
+                tmp = tempfile.NamedTemporaryFile( mode = "w", delete=False )
+                tmp.write( res['edit_content'] )
+                tmp.close()
+
+                subprocess.call( [ os.environ.get('EDITOR', '') or 'vi', tmp.name ] )
+
+                tmp = open(tmp.name, 'r')
+                updated_content = tmp.read()
+                tmp.close()
+                os.remove(tmp.name)
+
+                self.save_content(updated_content, token)
+            else:
+                print(res['cmd_out'])
+        else:
+            print("Error " + str(r.status_code) + ". Invalid URL?")
 
 
-parser = argparse.ArgumentParser(description="Send Chimpcom command")
-parser.add_argument("--token", action=argparse.BooleanOptionalAction)
-parser.add_argument("--url", default="https://deviouschimp.co.uk/api/respond")
-parser.add_argument("--username", nargs="?")
-parser.add_argument("command", nargs="*")
-args = parser.parse_args()
+    def save_content(self, content, token):
+        data = {
+            "action_id": self.action_id,
+            "cmd_in": "",
+            "content": content
+        }
 
-username = args.username
+        headers = {
+            "Authorization": "Bearer " + token,
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+            "X-Requested-With": "XMLHttpRequest",
+        }
 
-if not username:
-    username = input("Username: ")
+        print("Sending content...")
 
-service_id = "chimpcom"+args.url
-token = keyring.get_password(service_id, username)
+        r = requests.post(self.args.url, headers=headers, json=data)
+        res = r.json()
+        self.action_id = res['action_id']
 
-if not token or args.token:
-    token = getpass("Enter your token: ")
+        if r.status_code == 200:
+            print(res['cmd_out'])
+        else:
+            print("Error " + str(r.status_code) + ". Invalid URL?")
 
-if not token:
-    print("No token.")
-    sys.exit(1)
-
-keyring.set_password(service_id, username, token)
-
-# Single command
-if args.command:
-    get_response(" ".join(args.command), token)
-    sys.exit()
-
-# Interactive
-while True:
-    cmd_in = input("> ")
-    if cmd_in == "exit":
-        sys.exit()
-    get_response(cmd_in, token)
+cmd = Chimpcom()
